101ac005001cd870c1b2d791be2ba16e1c9d1bf4
yamato3310 <yamato3310.tuna@gmail.com>
2019-08-10 17:54:26 +0900

    masterの取り込み

---
+++ app/controllers/applicant_post/ApplicantPostController.scala
@@ -0,0 +1,51 @@
+package controllers.applicant_post
+
+import play.api.i18n.I18nSupport
+import play.api.mvc.{AbstractController, MessagesControllerComponents}
+// persistence: 永続化
+import persistence.employer_post.model.EmployerPost
+import persistence.employer_post.dao.EmployerPostDAO
+
+import persistence.employer.dao.EmployerDAO
+
+import persistence.geo.model.Location
+import persistence.geo.dao.LocationDAO
+// model
+import model.site.app.SiteViewValueEmployerPostIndex
+// import model.site.facility.SiteViewValueFacilityShow
+// import model.site.facility.SiteViewValueFacilityEdit
+// import model.site.facility.SiteViewValueFacilityAdd
+
+import model.component.util.ViewValuePageLayout
+import persistence.category.dao.CategoryDAO
+import model.site.app.SiteViewValueEmployerPostShow
+import persistence.category.model.Category
+
+
+
+// 施設
+//~~~~~~~~~~~~~~~~~~~~~
+class ApplicantPostController @javax.inject.Inject()(
+  // val: immutable, var: mutable
+  val employerPostDao: EmployerPostDAO,
+  val employerDao: EmployerDAO,
+  val daoLocation: LocationDAO,
+  val categoryDao: CategoryDAO,
+  cc: MessagesControllerComponents
+) extends AbstractController(cc) with I18nSupport {
+  implicit lazy val executionContext = defaultExecutionContext
+
+  def index = TODO
+
+  def add = TODO
+
+  def create = TODO
+
+  def show(id: Long) = TODO
+
+  def edit(id: Long) = TODO
+
+  def update(id: Long) = TODO
+
+  def destroy(id: Long) = TODO
+}
