0be59704cb0a80d36ff0bf12fac5d89fed4fbcd9
hirotosuzuki <hiroto7665@gmail.com>
2019-08-10 10:00:05 +0900

    [add]employer_postのControllerの骨格

---
+++ app/controllers/employer_post/EmployerPostController.scala
@@ -0,0 +1,43 @@
+package controllers.employer_post
+
+import play.api.i18n.I18nSupport
+import play.api.mvc.{AbstractController, MessagesControllerComponents}
+// persistence: 永続化
+import persistence.employer_post.model.EmployerPost
+import persistence.employer_post.dao.EmployerPostDAO
+
+import persistence.geo.model.Location
+import persistence.geo.dao.LocationDAO
+// model
+// import model.site.facility.SiteViewValueFacilityList
+// import model.site.facility.SiteViewValueFacilityShow
+// import model.site.facility.SiteViewValueFacilityEdit
+// import model.site.facility.SiteViewValueFacilityAdd
+
+import model.component.util.ViewValuePageLayout
+
+
+// 施設
+//~~~~~~~~~~~~~~~~~~~~~
+class EmployerPostController @javax.inject.Inject()(
+  // val: immutable, var: mutable
+  val employerPostDao: EmployerPostDAO,
+  val daoLocation: LocationDAO,
+  cc: MessagesControllerComponents
+) extends AbstractController(cc) with I18nSupport {
+  implicit lazy val executionContext = defaultExecutionContext
+
+  def index = TODO
+
+  def add = TODO
+
+  def create = TODO
+
+  def show(id: Long) = TODO
+
+  def edit(id: Long) = TODO
+
+  def update(id: Long) = TODO
+
+  def destroy(id: Long) = TODO
+}
