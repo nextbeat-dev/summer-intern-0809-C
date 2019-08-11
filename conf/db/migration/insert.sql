53a5f2434534ce5305b841bd51f8f08acfeda6f6
hirotosuzuki <hiroto7665@gmail.com>
2019-08-11 09:03:09 +0900

    [add]

--- conf/db/migration/insert.sql
+++ conf/db/migration/insert.sql
@@ -1,3 +1,4 @@
 INSERT INTO "employer"("location_id", "name", "address", "email", "phone") VALUES ('01000','hiroto','北海道','hiroto@gmail.com','000-0000-0000');
 
-INSERT INTO "employer_post"("employer_id", "location_id", "title", "address", "price", "job_date") VALUES ('1','01000','ミカンもぎたい人wanted','北海道', 10000,'2019-09-01');
+delete from "employer_post";
+VALUES ('1', '1','01000','ミカンもぎたい人wanted','北海道', 10000,'2019-09-01', '楽しく稼げる!', 2, 3, 4, 'google.com', 'google.com');
